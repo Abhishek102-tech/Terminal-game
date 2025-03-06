c5ab550cb145c4e1abd2c3d5d2692bd08e9ac7ae
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2025-03-06 23:42:11 +0530

    some changes

--- Snake game based on terminal/main.cpp
+++ Snake game based on terminal/main.cpp
@@ -3,6 +3,7 @@
 
 void playerMove(char player[],int position[2]);
 bool gameEnd(int position[2]);
+void foodChange(char food,int foodposition[2]);
 
 
 int main(){
@@ -11,6 +12,20 @@ int main(){
 
     columns= csbi.srWindow.Right - csbi.srWindow.Left + 1;
     rows= csbi.srWindow.Bottom - csbi.srWindow.Top + 1;  
+
+    while(true){
+
+    }
     
     return 0;
+}
+
+void playerMove(char player[],int position[2]){
+
+}
+bool gameEnd(int position[2]){
+
+}
+void foodChange(char food,int foodposition[2]){
+    
 }
