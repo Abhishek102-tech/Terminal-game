86ccd5bcb2d4bd25bf9277b696a9dc5be3150a30
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2025-03-06 18:51:49 +0530

    new game creating

---
+++ Snake game based on terminal/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+void playerMove(char player[],int position[2]);
+bool gameEnd(int position[2]);
+
+
+int main(){
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns,rows;
+
+    columns= csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows= csbi.srWindow.Bottom - csbi.srWindow.Top + 1;  
+    
+    return 0;
+}
